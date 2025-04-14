18b424d355e81ce55d84e2879da1bb5513efea35
Praveen Bhardwaj <itspraveenbhardwaj@gmail.com>
2025-04-14 16:52:11 +0000

    push 12

--- server.js
+++ server.js
@@ -2,25 +2,18 @@ const cds = require('@sap/cds');
 const express = require('express');
 const path = require('path');
 
-// Initialize Express app
 const app = express();
 
-// Bootstrap CAP services
 cds.on('bootstrap', () => {
-  // Serve UI5 frontend from /static
   app.use(express.static(path.join(__dirname, 'static')));
-
-  // Default route for root path
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
   });
 });
 
-// Serve CAP services from gen/srv
 (async () => {
-  await cds.serve('gen/srv').in(app);
-
-  // Start the server
+  // Serve CAP services from the srv/ directory
+  await cds.serve('srv').in(app);
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
